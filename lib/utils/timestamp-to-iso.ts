776e13f0795a381d5dc23f38c431059b4b30c428
bagdenius <bagdensparrow@gmail.com>
2026-03-22 23:56:14 +0200

    fix: fix time calc in Timestamp to ISO String util function

--- lib/utils/timestamp-to-iso.ts
+++ lib/utils/timestamp-to-iso.ts
@@ -1,5 +1,5 @@
 import { Timestamp } from '@vendee-cinema/contracts/gen/ts/google/protobuf/timestamp'
 
 export function timestampToISO(ts: Timestamp): string | null {
-	return new Date(ts.seconds * 100 + ts.nanos / 1_000_000).toISOString()
+	return new Date(ts.seconds * 1000 + ts.nanos / 1_000_000).toISOString()
 }
