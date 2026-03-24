fdb31b3f195fdf6160166546715f75c0f69029bf
bagdenius <bagdensparrow@gmail.com>
2026-03-24 12:32:57 +0200

    feat: add Session package to gRPC clients registry

--- lib/grpc/registry/grpc.registry.ts
+++ lib/grpc/registry/grpc.registry.ts
@@ -1,20 +1,25 @@
 import { PROTO_PATHS } from '@vendee-cinema/contracts'
 
 export const GRPC_CLIENTS = {
-	AUTH_PACKAGE: {
-		package: 'auth.v1',
-		protoPath: PROTO_PATHS.AUTH,
-		env: 'AUTH_GRPC_URL'
-	},
 	ACCOUNT_PACKAGE: {
 		package: 'account.v1',
 		protoPath: PROTO_PATHS.ACCOUNT,
 		env: 'AUTH_GRPC_URL'
 	},
-	USER_PACKAGE: {
-		package: 'user.v1',
-		protoPath: PROTO_PATHS.USER,
-		env: 'USER_GRPC_URL'
+	AUTH_PACKAGE: {
+		package: 'auth.v1',
+		protoPath: PROTO_PATHS.AUTH,
+		env: 'AUTH_GRPC_URL'
+	},
+	CATEGORY_PACKAGE: {
+		package: 'category.v1',
+		protoPath: PROTO_PATHS.CATEGORY,
+		env: 'MOVIE_GRPC_URL'
+	},
+	HALL_PACKAGE: {
+		package: 'hall.v1',
+		protoPath: PROTO_PATHS.HALL,
+		env: 'THEATER_GRPC_URL'
 	},
 	MEDIA_PACKAGE: {
 		package: 'media.v1',
@@ -26,24 +31,24 @@ export const GRPC_CLIENTS = {
 		protoPath: PROTO_PATHS.MOVIE,
 		env: 'MOVIE_GRPC_URL'
 	},
-	CATEGORY_PACKAGE: {
-		package: 'category.v1',
-		protoPath: PROTO_PATHS.CATEGORY,
-		env: 'MOVIE_GRPC_URL'
+	SEAT_PACKAGE: {
+		package: 'seat.v1',
+		protoPath: PROTO_PATHS.SEAT,
+		env: 'THEATER_GRPC_URL'
+	},
+	SESSION_PACKAGE: {
+		package: 'session.v1',
+		protoPath: PROTO_PATHS.SESSION,
+		env: 'SESSION_GRPC_URL'
 	},
 	THEATER_PACKAGE: {
 		package: 'theater.v1',
 		protoPath: PROTO_PATHS.THEATER,
 		env: 'THEATER_GRPC_URL'
 	},
-	HALL_PACKAGE: {
-		package: 'hall.v1',
-		protoPath: PROTO_PATHS.HALL,
-		env: 'THEATER_GRPC_URL'
-	},
-	SEAT_PACKAGE: {
-		package: 'seat.v1',
-		protoPath: PROTO_PATHS.SEAT,
-		env: 'THEATER_GRPC_URL'
+	USER_PACKAGE: {
+		package: 'user.v1',
+		protoPath: PROTO_PATHS.USER,
+		env: 'USER_GRPC_URL'
 	}
 } as const
